b61c01ef87ad7c44bd73648d18bf541da5631788
jayden <at3134165@126.com>
2015-05-11 15:34:09 +0800

    class version tool

---
+++ src/main/java/ClassVersion.java
@@ -0,0 +1,78 @@
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.*;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+import java.util.zip.ZipInputStream;
+
+/**
+ * Created by Administrator on 2015/5/11 15:30.
+ */
+public class ClassVersion {
+    public static void main(String[] args) throws Exception {
+        fileFolderVersion("D:\\work\\svn\\ta\\4-middleware\\CODE\\kafka\\core\\build\\classes\\main");
+
+        libFolderVersion("D:\\work\\svn\\ta\\4-middleware\\CODE\\storm-sample\\libs");
+    }
+
+    public static void libFolderVersion(String folder) throws IOException {
+        File dir = new File(folder);
+        Collection<File> jarFiles = FileUtils.listFiles(dir, new String[]{"jar"}, true);
+        Map<Integer, Set<String>> versionFiles = new TreeMap<Integer, Set<String>>();
+        for (File file : jarFiles) {
+            ZipInputStream zip = new ZipInputStream(new FileInputStream(file));
+            ZipFile zipFile = new ZipFile(file);
+            Enumeration<? extends ZipEntry> entries = zipFile.entries();
+            while (entries.hasMoreElements()) {
+                ZipEntry zipEntry =  entries.nextElement();
+                if (zipEntry.getName().endsWith("class")) {
+                    InputStream inputStream = zipFile.getInputStream(zipEntry);
+                    int v = getV(inputStream);
+                    Set<String> files = versionFiles.get(v);
+                    if (files == null) {
+                        files = new TreeSet<String>();
+                        versionFiles.put(v, files);
+                    }
+                    files.add(file.getAbsolutePath());
+                    break;
+                }
+            }
+        }
+
+        System.out.println(String.format("jar folder [%s] version:", folder));
+        for (Map.Entry<Integer, Set<String>> entry : versionFiles.entrySet()) {
+            System.out.println("version = " + entry.getKey());
+            for (String file : entry.getValue()) {
+                System.out.println(file);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void fileFolderVersion(String folder) throws IOException {
+        File dir = new File(folder);
+        Set<Integer> versions = new HashSet<Integer>();
+        Collection<File> files = FileUtils.listFiles(dir, new String[]{"class"}, true);
+        for (File file : files) {
+            int v = getV(file);
+            versions.add(v);
+        }
+        System.out.println(String.format("file folder [%s] version:%s\n", folder, versions));
+    }
+
+    private static int getV(File file) throws IOException {
+        FileInputStream stream = new FileInputStream(file);
+        return getV(stream);
+    }
+
+    private static int getV(InputStream stream) throws IOException {
+        byte[] ba = new byte[8];
+        int read = stream.read(ba);
+        return (int) ba[7];
+    }
+
+}
